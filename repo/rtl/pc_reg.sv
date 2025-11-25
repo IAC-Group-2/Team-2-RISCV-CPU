16e4e99247853f989c93ac9c3c463b36b988b7d5
Yichan Kim <yichankim@gmail.com>
2025-11-25 17:49:45 +0000

    Added suffix to variables in pc_reg.sv

--- repo/rtl/pc_reg.sv
+++ repo/rtl/pc_reg.sv
@@ -1,12 +1,12 @@
 module pc_reg #(
     parameter DATA_WIDTH = 32
 )(
-    input logic clk,
-    input logic rst,
-    input logic next_pc,
-    input logic [DATA_WIDTH-1:0] ImmOp,
+    input logic                     clk,
+    input logic                     rst,
+    input logic                     next_pc,
+    input logic [DATA_WIDTH-1:0]    ImmOp,
 
-    output logic [DATA_WIDTH-1:0] PC_o
+    output logic [DATA_WIDTH-1:0]   PC_o
 
 );
 
