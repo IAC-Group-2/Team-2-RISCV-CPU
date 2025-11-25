ba662ed0eeba31578eebd703a27c137a3cb2e3a9
Yichan Kim <yichankim@gmail.com>
2025-11-25 15:20:08 +0000

    Changed variable naming to have input/output suffix

--- repo/tb/tests/ALU_tb.cpp
+++ repo/tb/tests/ALU_tb.cpp
@@ -15,10 +15,10 @@ class TB_NAME : public BaseTestbench
 protected:
     void initializeInputs() override
     {
-        top->SrcA = 0;
-        top->SrcB = 0;
-        top->ALUControl = 0;
-        // outputs: ALUResult, Zero
+        top->SrcA_i = 0;
+        top->SrcB_i = 0;
+        top->ALUControl_i = 0;
+        // outputs: ALUResult_o, Zero_o
     }
 };
 
@@ -26,75 +26,75 @@ protected:
 // ADD
 TEST_F(TB_NAME, AddWorksTest)
 {
-    top->ALUControl = 0b000;   
-    top->SrcA = 10;
-    top->SrcB = 20;
+    top->ALUControl_i = 0b000;   
+    top->SrcA_i = 10;
+    top->SrcB_i = 20;
 
     top->eval();
 
-    EXPECT_EQ(top->ALUResult, 30);
-    EXPECT_EQ(top->Zero, 0);
+    EXPECT_EQ(top->ALUResult_o, 30);
+    EXPECT_EQ(top->Zero_o, 0);
 }
 
 // SUB
 TEST_F(TB_NAME, SubWorksTest)
 {
-    top->ALUControl = 0b001;   
-    top->SrcA = 20;
-    top->SrcB = 5;
+    top->ALUControl_i = 0b001;   
+    top->SrcA_i = 20;
+    top->SrcB_i = 5;
 
     top->eval();
 
-    EXPECT_EQ(top->ALUResult, 15);
+    EXPECT_EQ(top->ALUResult_o, 15);
 }
 
 // AND
 TEST_F(TB_NAME, AndWorksTest)
 {
-    top->ALUControl = 0b010;   
-    top->SrcA = 0b1100;
-    top->SrcB = 0b1010;
+    top->ALUControl_i = 0b010;   
+    top->SrcA_i = 0b1100;
+    top->SrcB_i = 0b1010;
 
     top->eval();
 
-    EXPECT_EQ(top->ALUResult, 0b1000);
+    EXPECT_EQ(top->ALUResult_o, 0b1000);
 }
 
 // OR
 TEST_F(TB_NAME, OrWorksTest)
 {
-    top->ALUControl = 0b011;   
-    top->SrcA = 0b1100;
-    top->SrcB = 0b0110;
+    top->ALUControl_i = 0b011;   
+    top->SrcA_i = 0b1100;
+    top->SrcB_i = 0b0110;
 
     top->eval();
 
-    EXPECT_EQ(top->ALUResult, 0b1110);
+    EXPECT_EQ(top->ALUResult_o, 0b1110);
 }
 
 // SLT (a < b)
 TEST_F(TB_NAME, SltWorksTest)
 {
-    top->ALUControl = 0b101;   
-    top->SrcA = 5;
-    top->SrcB = 9;
+    top->ALUControl_i = 0b101;   
+    top->SrcA_i = 5;
+    top->SrcB_i = 9;
 
     top->eval();
 
-    EXPECT_EQ(top->Zero, 1);
+    EXPECT_EQ(top->Zero_o, 1);
 }
 
 // undefined opcode
 TEST_F(TB_NAME, DefaultCaseTest)
 {
-    top->ALUControl = 0b111;   
-    top->SrcA = 1234;
-    top->SrcB = 5678;
+    top->ALUControl_i = 0b111;   
+    top->SrcA_i = 1234;
+    top->SrcB_i = 5678;
 
     top->eval();
 
-    EXPECT_EQ(top->ALUResult, 0);
-    EXPECT_EQ(top->Zero, 0);
+    EXPECT_EQ(top->ALUResult_o, 0);
+    EXPECT_EQ(top->Zero_o, 0);
 }
 
 
