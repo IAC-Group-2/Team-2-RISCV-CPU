467f88765d7ebb1e64349dac44a691586d60bd95
Yichan Kim <yichankim@gmail.com>
2025-11-25 19:21:29 +0000

    Added default casing for case, fixed incorrect suffixes when naming variables

--- repo/rtl/sign_extend.sv
+++ repo/rtl/sign_extend.sv
@@ -2,7 +2,7 @@ module sign_extend #(
     parameter DATA_WIDTH = 32
 )(
     input  logic [2:0]            imm_src_i,
-    input  logic [DATA_WIDTH-1:0] imm_instr_o,
+    input  logic [DATA_WIDTH-1:0] imm_instr_i,
     output logic [DATA_WIDTH-1:0] imm_ext_o
 );
 
@@ -11,42 +11,44 @@ always_comb begin
 
         // I-type
         3'b000: imm_ext_o = {
-            {20{imm_instr_o[31]}},
-            imm_instr_o[31:20]
+            {20{imm_instr_i[31]}},
+            imm_instr_i[31:20]
         };
 
         // B-type
         3'b001: imm_ext_o = {
-            {19{imm_instr_o[31]}}, 
-            imm_instr_0[31],
-            imm_instr_o[7],         
-            imm_instr_o[30:25],     
-            imm_instr_o[11:8],        
+            {19{imm_instr_i[31]}}, 
+            imm_instr_i[31],
+            imm_instr_i[7],         
+            imm_instr_i[30:25],     
+            imm_instr_i[11:8],        
             1'b0                      
         };
 
         // S-type
         3'b010: imm_ext_o = {
-            {20{imm_instr_o[31]}},
-            imm_instr_o[31:25],
-            imm_instr_o[11:7],
+            {20{imm_instr_i[31]}},
+            imm_instr_i[31:25],
+            imm_instr_i[11:7]
         };
 
         // U-type
         3'b011: imm_ext_o = {
-            {12{imm_instr_o[31]}},
-            imm_instr_o[31:12]
+            {12{imm_instr_i[31]}},
+            imm_instr_i[31:12]
         };
 
         // J-type
         3'b100: imm_ext_o = {
-            {11{imm_instr_o[31]}},
-            imm_instr_o[31],
-            imm_instr_o[19:12],
-            imm_instr_o[20],
-            imm_instr_o[30:21],
+            {11{imm_instr_i[31]}},
+            imm_instr_i[31],
+            imm_instr_i[19:12],
+            imm_instr_i[20],
+            imm_instr_i[30:21],
             1'b0         
         };
+
+        default: imm_ext_o = 0;
 
     endcase
 end
