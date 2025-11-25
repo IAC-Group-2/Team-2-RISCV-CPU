dba9eebaefab1d185a9947d59a2afe3773b1c498
carys <caryshcleung@gmail.com>
2025-11-25 15:46:45 +0000

    Updated naming convention

--- repo/rtl/control_unit.sv
+++ repo/rtl/control_unit.sv
@@ -1,51 +1,51 @@
 module control(
-    input   logic   [6:0]   op,
-    input   logic           Zero,
-    input   logic   [2:0]   funct3,
-    input   logic           funct7,
-    output  logic           RegWrite,
-    output  logic   [2:0]   ALUctrl,
-    output  logic           ALUsrc,
-    output  logic   [1:0]   Immsrc,
-    output  logic           PCsrc
+    input   logic   [6:0]   op_i,
+    input   logic           Zero_i,
+    input   logic   [2:0]   funct3_i,
+    input   logic           funct7_i,
+    output  logic           RegWrite_o,
+    output  logic   [2:0]   ALUctrl_o,
+    output  logic           ALUsrc_o,
+    output  logic   [2:0]   Immsrc_o,
+    output  logic           PCsrc_o
 );
 
 logic branch;
 
 always_comb begin
-    RegWrite = 0;
-    ALUctrl = 3'b0;
-    ALUsrc = 0;
-    Immsrc = 0;
+    RegWrite_o = 0;
+    ALUctrl_o = 3'b0;
+    ALUsrc_o = 0;
+    Immsrc_o = 0;
     branch = 0;
 
-    case (op) 
+    case (op_i) 
         7'b0010011: begin // addi
-            RegWrite = 1;
-            ALUctrl = 3'b0; // add
-            ALUsrc = 1; // immediate
-            Immsrc = 0; // I type
+            RegWrite_o = 1;
+            ALUctrl_o = 3'b0; // add
+            ALUsrc_o = 1; // immediate
+            Immsrc_o = 3'b0; // I type
             branch = 0;    
         end
 
         7'b1100011: begin // bne
-            RegWrite = 0;
-            ALUctrl = 3'b1; // sub
-            ALUsrc = 0; // reg
-            Immsrc = 1; // B type
+            RegWrite_o = 0;
+            ALUctrl_o = 3'b1; // sub
+            ALUsrc_o = 0; // reg
+            Immsrc_o = 3'b1; // B type
             branch = 1; // ben 
         end
         
         default: begin
-            RegWrite = 0;
-            ALUctrl = 3'b0;
-            ALUsrc = 0;
-            Immsrc = 0;
+            RegWrite_o = 0;
+            ALUctrl_o = 3'b0;
+            ALUsrc_o = 0;
+            Immsrc_o = 3'b0;
             branch = 0;
         end
     endcase
     
-    PCsrc = branch & ~Zero;
+    PCsrc_o = branch & ~Zero_i;
 
 end
 endmodule
