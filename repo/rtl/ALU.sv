2fc8acef8da93dbda27e451ce386fd0e8f13df16
Yichan Kim <yichankim@gmail.com>
2025-11-25 15:21:15 +0000

    Changed variable naming to have input/output suffix

--- repo/rtl/ALU.sv
+++ repo/rtl/ALU.sv
@@ -1,25 +1,25 @@
 module ALU #(
     DATA_WIDTH = 32
 ) (
-    input   logic [DATA_WIDTH-1:0]  SrcA,
-    input   logic [DATA_WIDTH-1:0]  SrcB,
-    input   logic [2:0]             ALUControl,
-    output  logic [DATA_WIDTH-1:0]  ALUResult,
-    output  logic                   Zero
+    input   logic [DATA_WIDTH-1:0]  SrcA_i,
+    input   logic [DATA_WIDTH-1:0]  SrcB_i,
+    input   logic [2:0]             ALUControl_i,
+    output  logic [DATA_WIDTH-1:0]  ALUResult_o,
+    output  logic                   Zero_o
 );
 always_comb begin
-    ALUResult = 0;
-    Zero = 0;
-    case (ALUControl) //codes given in lecture notes
-        3'b000: ALUResult = SrcA + SrcB; //add
-        3'b001: ALUResult = SrcA - SrcB; //subtract
-        3'b101: Zero = SrcA < SrcB; //set less than
-        3'b011: ALUResult = SrcA | SrcB; //bitwise or
-        3'b010: ALUResult = SrcA & SrcB; //bitwise and
+    ALUResult_o = 0;
+    Zero_o = 0;
+    case (ALUControl_i) //codes given in lecture notes
+        3'b000: ALUResult_o = SrcA_i + SrcB_i; //add
+        3'b001: ALUResult_o = SrcA_i - SrcB_i; //subtract
+        3'b101: Zero_o = SrcA_i < SrcB_i; //set less than
+        3'b011: ALUResult_o = SrcA_i | SrcB_i; //bitwise or
+        3'b010: ALUResult_o = SrcA_i & SrcB_i; //bitwise and
         default:
             begin
-            Zero = 0;
-            ALUResult = 0;
+            Zero_o = 0;
+            ALUResult_o = 0;
             end
     endcase
 end
