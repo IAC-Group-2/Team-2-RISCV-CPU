944a63337ba912696678b72b5e0e0298dfcb9be4
Yichan Kim <yichankim@gmail.com>
2025-11-25 19:19:57 +0000

    Fixed bug: Incorrect referencing of 0

--- repo/rtl/pc_reg.sv
+++ repo/rtl/pc_reg.sv
@@ -3,13 +3,13 @@ module pc_reg #(
 )(
     input logic                     clk_i,
     input logic                     rst_i,
-    input logic                     pcNext_i,
+    input logic  [DATA_WIDTH-1:0]                      pcNext_i,
     output logic [DATA_WIDTH-1:0]   pc_o
 );
 
 always_ff @(posedge clk_i, posedge rst_i) 
     if (rst_i) 
-        pc_o <= '0;
+        pc_o <= 0;
     else 
         pc_o <= pcNext_i;
 
