fc0606ed0b0b42fd182e0f3b42a4f86050a3f606
carys <caryshcleung@gmail.com>
2025-12-01 00:28:37 +0000

    Updated single cycle adder

--- repo/rtl/addr.sv
+++ repo/rtl/addr.sv
@@ -1,16 +1,14 @@
 module addr #(
     parameter DATA_WIDTH = 32
 )(
-    input logic [DATA_WIDTH-1:0]        PCF_i,
-    input logic [DATA_WIDTH-1:0]        PCE_i,
-
+    input logic [DATA_WIDTH-1:0]        PC_i,
     input logic [DATA_WIDTH-1:0]        ImmOp_i,
-    output logic [DATA_WIDTH-1:0]       PCTarget_o,
-    output logic [DATA_WIDTH-1:0]       PCPlus4_o
+    output logic [DATA_WIDTH-1:0]       branch_PC_o,
+    output logic [DATA_WIDTH-1:0]       inc_PC_o
 );
     always_comb begin
-        PCTarget_o  = PCE_i + ImmOp_i;
-        PCPlus4_o   = PCF_i + 'd4;
+        branch_PC_o = PC_i + ImmOp_i;
+        inc_PC_o    = PC_i + 'd4;
     end
-
+      
 endmodule
