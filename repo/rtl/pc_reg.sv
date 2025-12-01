b5f955f03d117b053bd5f36adb1d5a138109b1b8
carys <caryshcleung@gmail.com>
2025-12-01 01:20:20 +0000

    Updated single cycle PC register

--- repo/rtl/pc_reg.sv
+++ repo/rtl/pc_reg.sv
@@ -1,19 +1,17 @@
 module pc_reg #(
     parameter DATA_WIDTH = 32
 )(
-    input logic                     clk_i,
-    input logic                     rst_i,
-    input logic                     en_i,
-    input logic  [DATA_WIDTH-1:0]   PCNext_i,
-    output logic [DATA_WIDTH-1:0]   PC_o
+    input  logic                   clk_i,
+    input  logic                   rst_i,
+    input  logic [DATA_WIDTH-1:0]  next_pc_i,
+    output logic [DATA_WIDTH-1:0]  PC_o
 );
 
-always_ff @(posedge clk_i, posedge rst_i) 
-    if (rst_i) 
-        PC_o    <= 0;
-    else if (en_i)
-        PC_o    <= PCNext_i;
-    else //freeze
-        PC_o    <= PC_o; 
+    always_ff @(posedge clk_i, posedge rst_i) begin
+        if (rst_i)
+            PC_o <= {DATA_WIDTH{1'b0}};
+        else
+            PC_o <= next_pc_i;
+    end
 
 endmodule
