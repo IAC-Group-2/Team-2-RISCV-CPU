996a0035fb9085e08b4bca6fe300420e255a0664
carys <caryshcleung@gmail.com>
2025-12-01 00:51:09 +0000

    minor update to fix syntax and follow good design practices

--- repo/rtl/ALU.sv
+++ repo/rtl/ALU.sv
@@ -1,6 +1,6 @@
 module ALU #(
     parameter DATA_WIDTH = 32
-) (
+)(
     input   logic [DATA_WIDTH-1:0]  SrcA_i,
     input   logic [DATA_WIDTH-1:0]  SrcB_i,
     input   logic [2:0]             ALUControl_i,
@@ -9,7 +9,7 @@ module ALU #(
 );
 
     always_comb begin
-        ALUResult_o = '0;
+        ALUResult_o = {DATA_WIDTH{1'b0}};
         case (ALUControl_i)
             3'b000: ALUResult_o = SrcA_i + SrcB_i; // ADD
             3'b001: ALUResult_o = SrcA_i - SrcB_i; // SUB
@@ -19,11 +19,11 @@ module ALU #(
             3'b110: ALUResult_o = SrcA_i << SrcB_i[4:0]; // SLLI
             3'b101: begin // SLT
                 if ($signed(SrcA_i) < $signed(SrcB_i)) 
-                    ALUResult_o = 32'd1;
+                    ALUResult_o = {{DATA_WIDTH-1{1'b0}}, 1'b1};
                 else 
-                    ALUResult_o = 32'd0;
+                    ALUResult_o = {DATA_WIDTH{1'b0}};
             end
-            default: ALUResult_o = '0;
+            default: ALUResult_o = {DATA_WIDTH{1'b0}};
         endcase
     end
 
