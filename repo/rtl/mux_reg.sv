b857fe8ed309abbcd1b4cbd9ee664fa646b928a3
carys <caryshcleung@gmail.com>
2025-12-02 23:38:48 +0000

    Updated PC mux to support pipelining

--- repo/rtl/mux_reg.sv
+++ repo/rtl/mux_reg.sv
@@ -1,11 +1,22 @@
 module mux_reg #(
-    DATA_WIDTH = 32
-) (
-    input   logic [DATA_WIDTH-1:0]  inc_PC,
-    input   logic [DATA_WIDTH-1:0]  branch_PC,
-    input   logic                   PC_src,
-    output  logic [DATA_WIDTH-1:0]  next_PC
+    parameter DATA_WIDTH = 32
+)(
+    input  logic [DATA_WIDTH-1:0] PCPlus4F_i,
+    input  logic [DATA_WIDTH-1:0] PCTargetE_i,
+    input  logic [DATA_WIDTH-1:0] ALUResultE_i,
+    input  logic                  PCSrcE_i,
+    input  logic                  JalrE_i, // from top: (JumpE && ALUSrcE)
+    output logic [DATA_WIDTH-1:0] PCNext_o
 );
-    assign next_PC = PC_src ? branch_PC : inc_PC;
+
+    always_comb begin
+        if (PCSrcE_i) begin // If jump/branch taken
+            // JALR uses ALU Result, others use PC adder target
+            PCNext_o = (JalrE_i) ? ALUResultE_i : PCTargetE_i;
+        end else begin
+            // Step through normally
+            PCNext_o = PCPlus4F_i;
+        end
+    end
 
 endmodule
