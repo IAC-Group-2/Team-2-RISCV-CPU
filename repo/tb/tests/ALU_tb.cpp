1cf7040907fb5e74af49632653ba8ab955b0de18
carys <caryshcleung@gmail.com>
2025-12-05 10:45:59 +0000

    Updated ALU testbench to test full base instruction (R, S, U)

--- repo/tb/tests/ALU_tb.cpp
+++ repo/tb/tests/ALU_tb.cpp
@@ -26,7 +26,7 @@ protected:
 // ADD
 TEST_F(TB_NAME, AddWorksTest)
 {
-    top->ALUControl_i = 0b000;   
+    top->ALUControl_i = 0b0000;   
     top->SrcA_i = 10;
     top->SrcB_i = 20;
 
@@ -39,19 +39,20 @@ TEST_F(TB_NAME, AddWorksTest)
 // SUB
 TEST_F(TB_NAME, SubWorksTest)
 {
-    top->ALUControl_i = 0b001;   
+    top->ALUControl_i = 0b0001;   
     top->SrcA_i = 20;
     top->SrcB_i = 5;
 
     top->eval();
 
     EXPECT_EQ(top->ALUResult_o, 15);
+    EXPECT_EQ(top->Zero_o, 0);
 }
 
 // AND
 TEST_F(TB_NAME, AndWorksTest)
 {
-    top->ALUControl_i = 0b010;   
+    top->ALUControl_i = 0b0010;   
     top->SrcA_i = 0b1100;
     top->SrcB_i = 0b1010;
 
@@ -63,7 +64,7 @@ TEST_F(TB_NAME, AndWorksTest)
 // OR
 TEST_F(TB_NAME, OrWorksTest)
 {
-    top->ALUControl_i = 0b011;   
+    top->ALUControl_i = 0b0011;   
     top->SrcA_i = 0b1100;
     top->SrcB_i = 0b0110;
 
@@ -72,16 +73,77 @@ TEST_F(TB_NAME, OrWorksTest)
     EXPECT_EQ(top->ALUResult_o, 0b1110);
 }
 
-// SLT (a < b)
+// SLL
+TEST_F(TB_NAME, SllWorksTest)
+{
+    top->ALUControl_i = 0b0101;   
+    top->SrcA_i = 1;
+    top->SrcB_i = 4;
+
+    top->eval();
+
+    EXPECT_EQ(top->ALUResult_o, 16);
+}
+
+// SRL
+TEST_F(TB_NAME, SrlWorksTest)
+{
+    top->ALUControl_i = 0b0110;
+    top->SrcA_i = 0xF0000000;
+    top->SrcB_i = 4;
+
+    top->eval();
+
+    EXPECT_EQ(top->ALUResult_o, 0x0F000000);
+}
+
+// SRA
+TEST_F(TB_NAME, SraWorksTest)
+{
+    top->ALUControl_i = 0b0111;
+    top->SrcA_i = 0xF0000000;
+    top->SrcB_i = 4;
+
+    top->eval();
+
+    EXPECT_EQ(top->ALUResult_o, 0xFF000000);
+}
+
+// SLT (1000)
 TEST_F(TB_NAME, SltWorksTest)
 {
-    top->ALUControl_i = 0b101;   
+    top->ALUControl_i = 0b1000;   
     top->SrcA_i = 5;
     top->SrcB_i = 9;
 
     top->eval();
 
-    EXPECT_EQ(top->Zero_o, 1);
+    EXPECT_EQ(top->ALUResult_o, 1);
+    EXPECT_EQ(top->Zero_o, 0);
+}
+
+// SLTU
+TEST_F(TB_NAME, SltuWorksTest)
+{
+    top->ALUControl_i = 0b1001;   
+    top->SrcA_i = 1;
+    top->SrcB_i = -1; 
+
+    top->eval();
+
+    EXPECT_EQ(top->ALUResult_o, 1);
+}
+
+// LUI Pass-Through (1111)
+TEST_F(TB_NAME, LuiWorksTest)
+{
+    top->ALUControl_i = 0b1111;   
+    top->SrcA_i = 0x12345678; // Should be ignored
+    top->SrcB_i = 0xABCDEF00; // Should be passed
+
+    top->eval();
+
+    EXPECT_EQ(top->ALUResult_o, 0xABCDEF00);
 }
 
 // undefined opcode
@@ -94,7 +156,7 @@ TEST_F(TB_NAME, DefaultCaseTest)
     top->eval();
 
     EXPECT_EQ(top->ALUResult_o, 0);
-    EXPECT_EQ(top->Zero_o, 0);
+    EXPECT_EQ(top->Zero_o, 1);
 }
 
 
